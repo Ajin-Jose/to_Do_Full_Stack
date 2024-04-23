cdbdab6ef69c490136c8d4ce5d2cd4b1f141e23d
Ajin-Jose <joseajin01@gmail.com>
2024-04-23 10:19:50 +0530

    v1.02

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+// import { createTodo, updateTodo } from './types.js';
+const { createTodo, updateTodo } =  require('./types.js');
+
 const port = 3000;
 
 const app = express();
 app.use(express.json());
 app.listen(port);
+
+
 
 // Creating a Schema
 
@@ -12,9 +17,30 @@ app.get('/', (req,res) => {
 })
 
 app.post('/todo', (req,res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success)
+     {
+        res.status(411).json({
+            msg : "You sent the wrong inputs",
+        })
+        return;
+     }
 
+    // On Success, Transfer it to Mongo DB
 })
 
-app.post('/completed', (req,res) => {
 
+app.post('/completed', (req,res) => {
+    const update_Payload = req.body;
+    const parsed_Playload = updateTodo.safeParse(update_Payload);
+    if(!parsed_Playload.success)
+     {
+        res.status(411).json({
+            msg : "You sent the wrong inputs",
+        })
+        return;
+     }
+
+    //  On Success, Transfer to MongoDB
 })
